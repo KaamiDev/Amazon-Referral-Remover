caa27c7eddf848831e86473362b9f3e7c6fe7be6
Kaami Dev <kaami@kaamidev.com>
2020-09-03 00:23:54 -0400

    added reshortern function

--- app.js
+++ app.js
@@ -16,5 +16,35 @@ const removeReferral = (url) => {
 	});
 };
 
+// declare reshorten function
+const reshorten = (url) => {
+	return new Promise((resolve) => {
+		// shorten url once again using bit.ly
+		axios
+			.post(
+				'https://api-ssl.bitly.com/v4/shorten',
+				{
+					long_url: newUrl,
+					domain: 'bit.ly',
+					group_guid: process.env.GROUP_GUID
+				},
+				{
+					headers: {
+						Authorization: 'Bearer ' + process.env.BITLY_TOKEN,
+						'Content-Type': 'application/json'
+					}
+				}
+			)
+			.then((response) => {
+				// return new shortened url
+				resolve(response.data.link);
+			})
+			.catch((err) => {
+				// catch and log error(s)
+				console.log(err);
+			});
+	});
+};
+
 // export remove function
 module.exports = removeReferral;
